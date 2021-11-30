9cc67778d377f1ffa26d6f42a9931ec2c55d3d14
Leonardo Rocca <lrocca@student.42roma.it>
2021-11-30 12:09:37 +0100

    ex03: fix compilation for AMateria, now canonical
    AMateria::use is now a pure method as it's implementation-specific

--- ex03/AMateria.cpp
+++ ex03/AMateria.cpp
@@ -6,7 +6,7 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 21:05:14 by lrocca            #+#    #+#             */
-/*   Updated: 2021/11/27 17:09:41 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/11/30 11:59:59 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,15 @@
 
 #include "AMateria.hpp"
 
-AMateria::AMateria(const std::string& type): type(type) {
-	std::cout << "AMateria constructor called" << std::endl;
+AMateria::AMateria() {};
+
+AMateria::AMateria(const AMateria& other): type(other.type) {};
+
+AMateria::AMateria(const std::string& type): type(type) {}
+
+AMateria&	AMateria::operator=(const AMateria& other) {
+	if (this != &other) {}
+	return *this;
 }
 
 const std::string&	AMateria::getType() const {
--- ex03/AMateria.hpp
+++ ex03/AMateria.hpp
@@ -6,7 +6,7 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 21:00:31 by lrocca            #+#    #+#             */
-/*   Updated: 2021/11/27 19:20:53 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/11/30 12:04:26 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,21 @@
 class ICharacter;
 
 class AMateria {
+	private:
+		AMateria();
 	protected:
 		std::string	type;
 	public:
-		AMateria() {};
+		AMateria(const AMateria& other);
 		AMateria(const std::string& type);
 		virtual ~AMateria() {};
+
+		AMateria&	operator=(const AMateria& other);
 
 		const std::string&	getType(void) const;
 
 		virtual AMateria*	clone(void) const = 0;
-		virtual void		use(ICharacter& target);
+		virtual void		use(ICharacter& target) = 0;
 };
 
 #endif
